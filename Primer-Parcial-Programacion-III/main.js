78beef7c44f6f530bfbaf9be0aab0a0341c1509c
Pablo Guillermo <pablodanielguillermo@gmail.com>
2024-10-16 22:10:00 -0300

    .

--- Primer-Parcial-Programacion-III/main.js
+++ Primer-Parcial-Programacion-III/main.js
@@ -6,30 +6,30 @@ let divBusqueda = document.getElementById("busqueda");
 let inputTraerCiudad = document.getElementById("traer-ciudad");
 let botonBusqueda = document.getElementById("buscar");
 
-let ciudadBuscada = inputTraerCiudad.value;
 
 botonBusqueda.addEventListener("click", (ciudadBuscada) => traerCiudad(ciudadBuscada));
 
 window.onload = async () => {
     const respuesta = await fetch("https://weather-api-progra-3.vercel.app/weather/avellaneda");
-
+    
     const json = await respuesta.json();
-
+    
     let clima = Clima.createFromJsonString(JSON.stringify(json));
-
+    
     let elementoClima = clima.createHtmlElement();
-
+    
     for (let i = 0; i < clima.forecast.length; i++) {
         let pronostico = new Pronostico(clima.forecast[i].day, clima.forecast[i].temperature, clima.forecast[i].wind)
-
+        
         elementoClima.appendChild(pronostico.createHtmlElement());
     }
-
+    
     divClima.appendChild(elementoClima);
 }
 
 async function traerCiudad(ciudadABuscar) {
-    const respuesta = await fetch(`https://weather-api-progra-3.vercel.app/weather/${ciudadABuscar}`);
+    let ciudadBuscada = inputTraerCiudad.value;
+    const respuesta = await fetch(`https://weather-api-progra-3.vercel.app/weather/${ciudadBuscada}`);
 
     const json = await respuesta.json();
 
